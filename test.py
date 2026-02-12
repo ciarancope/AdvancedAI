b190b25f34e23b5d7331f44df3872637fc20c514
Nathaniel <nathaniel@im-me.com>
2026-02-12 11:57:51 +0000

    Added stupid tetris game

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-import stable_retro
-env = stable_retro.make(game='Airstriker-Genesis=v0')
+import gymnasium as gym
+import ale_py
+from gymnasium.utils.play import play
+
+gym.register_envs(ale_py)
+
+play(gym.make("ALE/Tetris-v5", render_mode="rgb_array"))
 
